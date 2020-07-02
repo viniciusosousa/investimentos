b3e26ed0936b110f52e8b6cc678a282dc8a48bbc
Vinicius Oliveira <vinicius.o.sousa@hotmail.com>
2020-07-02 17:49:26 -0300

    Index ajustado para forcar path em minusculas

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ spl_autoload_register(function (String $class)
 	$vendor = 'vini';
 	$file = str_replace($vendor, __DIR__, $class).'.php';
 
-    $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
+    $file = strtolower(str_replace('\\', DIRECTORY_SEPARATOR, $file));
     //echo "\r\npath: ".$file."\n";
 	if (file_exists($file)) {
         require($file);
