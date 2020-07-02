1fb69327bb7b1a506a284477fd82b2367d08d60b
Vinicius Oliveira <vinicius.o.sousa@hotmail.com>
2020-07-02 12:06:24 -0300

    Automatizacao da carga do site da B3

--- cargas/carga.php
+++ cargas/carga.php
@@ -1,5 +1,6 @@
 <?php use vini\app\Model;
-//error_reporting(E_ALL);?>
+include_once('../app/model.php');
+error_reporting(E_ALL);?>
 <DOCTYPE! html>
 <html lang="pt-BR">
 <head>
@@ -31,25 +32,60 @@
     </form>
     </div>
     <?php
+
     if (isset($_FILES['arquivo'])){
-    //print_r($_FILES);
-    if($_FILES['arquivo']['type']=='application/zip'){
-            echo 'Descompactando arquivo....';
+      //print_r($_FILES);
+      if($_FILES['arquivo']['type']=='application/zip'){
+          processaZip($_FILES["arquivo"]["tmp_name"]);
+      } else {
+          echo '<span style="color: red">Ocorreu um erro</span>';
+      }
+	}
+
+
+	const SEL_CORTE	   = "SELECT ULTM_FOTO_PRGO FROM financas.vw_ultimo_foto";
+
+	$corte = new Model;
+	$corte->sql = SEL_CORTE;
+	$corte->query();
+	$corte->fetchRow();
+	$dataCorte = \DateTime::createFromFormat('Y-m-d',$corte->linhas[0]);
+	//$dataCorte->sub(new \DateInterval('P1D'));
+	echo "Ultimo dia carregado: ". $dataCorte->format('d/m/Y').'<br>';
+	$dataAtual = new \DateTime;
+	$diferenca = $dataAtual->diff($dataCorte);
+	if($diferenca->d >=2 || ($diferenca->d==1 && $diferenca->h >= 20)){
+		for($i=1; $i <= $diferenca->d; $i++ ){
+			$file = 'COTAHIST_D'.$dataCorte->add(new \DateInterval('P1D'))->format('dmY').'.ZIP';
+			$path = 'http://bvmf.bmfbovespa.com.br/InstDados/SerHist/'.$file;
+			$handle = @fopen($path, 'r');
+			if ($handle){
+			echo 'Baixando '.$file.'<br>';
+			copy($path, $file);
+			processaZip($file);
+			Model::exeSqlFile('sql/bovespa.sql');
+			}
+			Model::exeSqlFile('sql/bovespa_atualiza_precos.sql');
+		}
+	}
+	function processaZip($arquivo){
+		    echo 'Descompactando arquivo....';
             $zip = new ZipArchive;
-            $res = $zip->open($_FILES["arquivo"]["tmp_name"]);
+            $res = $zip->open($arquivo);
             if ($res === TRUE) {
                 $zip->extractTo('txt');
                 $name = $zip->getNameIndex(0);
                 if (file_exists('txt/COTAHIST.TXT')) unlink('txt/COTAHIST.TXT');
                 rename('txt/'.$name, 'txt/COTAHIST.TXT');
                 $zip->close();
-				unlink('/data/data/com.termux/files/home/storage/shared/Download/'.$_FILES["arquivo"]["name"]);
+				if (isset($_FILES["arquivo"]["name"])){
+					$ziporiginal = '/data/data/com.termux/files/home/storage/shared/Download/'.$_FILES["arquivo"]["name"];
+					if (file_exists($ziporiginal))
+						unlink($ziporiginal);
+				}
                 echo '<span style="color: green">ok</span><br><br>';
-            } else {
-                echo '<span style="color: red">Ocorreu um erro</span>';
-            }
 
-        }
+	}
 }
 
 
@@ -60,10 +96,12 @@
 
 if ( (isset($_GET['carga']) or isset($_POST['carga'])) ){
     $script = ($_GET['carga'] ?:  $_POST['carga']);
-    include_once('../app/model.php');
     echo '<p style="color: green">Conexão feita com sucesso</p>';
     $filename = __DIR__.DIRECTORY_SEPARATOR.'sql'.DIRECTORY_SEPARATOR.$script.'.sql';
     Model::exeSqlFile($filename);
-	echo "******** FIM DO SCRIPT *********"
+	echo "******** FIM DO SCRIPT *********";
 }
 ?>
+
+<?php
+//http://bvmf.bmfbovespa.com.br/InstDados/SerHist/COTAHIST_A2020.ZIP?>
